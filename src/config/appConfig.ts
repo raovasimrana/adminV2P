9f0124f22ee20ac0769770343eff842b8b540c24
chetan4635 <chetan.singh@mail.vinove.com>
2019-02-05 23:31:35 +0530

    URL Chnage

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
-    baseUrl: 'http://35.172.113.52:3090',
-    fileUrl: 'http://35.172.113.52:3090/api/document/download'
+    baseUrl: 'http://vet4pet.app/api',
+    fileUrl: 'http://vet4pet.app/api/document/download'
     // baseUrl: 'http://localhost:3090'
 };
