8bd1494fb2967f66074067ef5bc58dc62342a242
chetan4635 <jaiwas009@gmail.com>
2019-02-09 10:37:32 +0530

    URL Change

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
-    baseUrl: 'http://vet4pet.app/api',
+    baseUrl: 'http://vet4pet.app',
     fileUrl: 'http://vet4pet.app/api/document/download'
     // baseUrl: 'http://localhost:3090'
 };
